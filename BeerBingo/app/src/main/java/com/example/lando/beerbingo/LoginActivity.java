7c5d84bab77b2e387604aeb791a15601bf22849d
Emilio Lando <emilio.lando@gmail.com>
2017-03-04 13:39:02 +0100

    loginstuff

--- BeerBingo/app/src/main/java/com/example/lando/beerbingo/LoginActivity.java
+++ BeerBingo/app/src/main/java/com/example/lando/beerbingo/LoginActivity.java
@@ -3,11 +3,10 @@ package com.example.lando.beerbingo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
