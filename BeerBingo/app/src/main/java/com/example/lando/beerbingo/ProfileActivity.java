6f79d7733a67291d6d3e8410e307eeb008ef23b2
Katja Röös <katja7667@gmail.com>
2017-03-04 16:22:28 +0100

    Gets name for profile from database.

--- BeerBingo/app/src/main/java/com/example/lando/beerbingo/ProfileActivity.java
+++ BeerBingo/app/src/main/java/com/example/lando/beerbingo/ProfileActivity.java
@@ -1,17 +1,24 @@
 package com.example.lando.beerbingo;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
-import static com.example.lando.beerbingo.R.id.avatar;
-import static com.example.lando.beerbingo.R.id.image;
+
+import com.google.firebase.database.*;
+
+
 
 public class ProfileActivity extends AppCompatActivity {
+    private FirebaseDatabase mDatabase;
+    private static final String TAG = "ProfileActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,7 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         load_image();
         load_user_name();
+        //TODO User information generator
     }
     private void load_image(){
         Toast.makeText(getApplicationContext(), "Loading your avatar.",
@@ -30,9 +38,31 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
     private void load_user_name(){
-        TextView textView = (TextView) findViewById(R.id.user_name);
+        final TextView textView = (TextView) findViewById(R.id.user_name);
         //TODO get username from somewhere
-        textView.setText("Katja");
+        mDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = mDatabase.getReference();
+
+        //myRef.setValue("hej");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //Toast.makeText(ProfileActivity.this, dataSnapshot.child("users/katja").getValue().toString(),
+                  //      Toast.LENGTH_SHORT).show();
+                textView.setText(dataSnapshot.child("users/katja").getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
 
     }
 }
