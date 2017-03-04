b88c9b6ab52c6e6e813e88da395322b16de0ef5d
Emilio Lando <emilio.lando@gmail.com>
2017-03-04 03:54:49 +0100

    challenge list with switching between challenge views

--- BeerBingo/app/src/main/java/com/example/lando/beerbingo/ChallengeActivity.java
+++ BeerBingo/app/src/main/java/com/example/lando/beerbingo/ChallengeActivity.java
@@ -2,6 +2,8 @@ package com.example.lando.beerbingo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ChallengeActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ChallengeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge);
+        TextView tv = new TextView(this);
+        String s = getIntent().getExtras().getString("key");
+        tv.setText(s);
+        LinearLayout l = (LinearLayout) findViewById(R.id.activityChallengeContainer);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        l.addView(tv, lp);
     }
 }
